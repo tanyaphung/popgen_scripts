22b156e1947695ae1892f612256bfd53964a2ec1
tnphung <tnphung@alum.mit.edu>
2016-12-22 11:22:09 -0800

    Add functionality for arguments

--- Group_Sites_Into_Loci/scripts/python/GroupSites_XkbLoci.py
+++ Group_Sites_Into_Loci/scripts/python/GroupSites_XkbLoci.py
@@ -1,6 +1,25 @@
 import sys
 import os
+import argparse
+import csv
 
+def parse_args():
+    """
+    Parse command-line arguments
+    """
+    parser = argparse.ArgumentParser(description="This script generates a collection of of Xkb loci where Xkb is defined by the user")
+        
+    parser.add_argument(
+            "--target_bed", nargs="*",required=True,
+            help="REQUIRED. Bed files (no header) containing the regions to be grouped")
+    parser.add_argument(
+            "--desired_loci_length", type=int, required=True,
+            help="REQUIRED. Length of the loci. Examples are 50000 for 50kb loci, 100000 for 100kb loci")
+    parser.add_argument("--outfile", required=True, 
+                    help="REQUIRED. Name of output file.")
+
+    args = parser.parse_args()
+    return args
 
 def group_sites_XkbLoci(intervals, Xkb):
     all_loci = {}
@@ -28,19 +47,29 @@ def group_sites_XkbLoci(intervals, Xkb):
                     intervals.insert(0, left)
         all_loci[count] = current_loci
         count += 1
-    for key, value in all_loci.iteritems():
-        for each_value in value:
-            to_print_list = [key, each_value[0], each_value[1]]
-            print('\t'.join([str(x) for x in to_print_list]))
-
+    return all_loci
 
 def main():
-    """ Usage """
+    # Grab arguments
+    args = parse_args()
     intervals = []
-    with open(sys.argv[1]) as intervalsFile:
+    with open(args.target_bed[0],"r") as intervalsFile:
         for line in intervalsFile:
             line = line.split("\t")
             intervals.append((int(line[1]), int(line[2]) - 1))
-    Xkb = int(sys.argv[2])
-    group_sites_XkbLoci(intervals, Xkb)
+
+    Xkb = args.desired_loci_length
+    all_loci = group_sites_XkbLoci(intervals, Xkb)
+    with open(args.outfile,"w") as f:
+        output_list = []
+        for key, value in all_loci.iteritems():
+            key_all_values = []
+            for each_value in value:
+                key_all_values.append(each_value[0])
+                key_all_values.append(each_value[1])
+            to_print_list = [key, min(key_all_values), max(key_all_values)]
+            output_list.append(to_print_list)
+
+        w = csv.writer(f)
+        w.writerows(output_list)
 main()
