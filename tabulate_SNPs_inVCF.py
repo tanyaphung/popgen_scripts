72ef072cea6488b828d694a4dceb88c11868da5b
tnphung <tnphung@ucla.edu>
2017-05-09 09:49:51 -0700

    Add functionality to also accept gzipped VCFs

--- tabulate_SNPs_inVCF.py
+++ tabulate_SNPs_inVCF.py
@@ -1,16 +1,22 @@
 import os
 import sys
 import argparse
+import gzip
 
 def parse_args():
 	"""
 	Parse command-line arguments
 	"""
-	parser = argparse.ArgumentParser(description="This script tabulates the number of SNPs in each VCF file. This script takes in (1) a VCF file and returns a number indicating the number of SNPs")
+	parser = argparse.ArgumentParser(description="This script tabulates the number of SNPs in each VCF file. This script takes in (1) a VCF file and returns a number indicating the number of SNPs and (2) a string to indicate whether the VCF is gzipped or not.")
 
 	parser.add_argument(
             "--VCF", required=True,
             help="REQUIRED. Input the path to the VCF file")
+
+	parser.add_argument(
+            "--compressed", required=True,
+            help="REQUIRED. Please indicate gzip or not_gzip.")
+
 	args = parser.parse_args()
 	return args
 
@@ -18,9 +24,15 @@ def main():
 	args = parse_args()
 
 	count = 0
-	with open(args.VCF, "r") as f:
-		for line in f:
-			if not line.startswith("#"):
-				count += 1
+	if args.compressed == 'not_gzip':
+		with open(args.VCF, "r") as f:
+			for line in f:
+				if not line.startswith("#"):
+					count += 1
+	if args.compressed == 'gzip':
+		with gzip.open(args.VCF, "r") as f:
+			for line in f:
+				if not line.startswith("#"):
+					count += 1
 	print count
 main()
